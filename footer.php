6ef46838ae5c0f6ce1fadd3f661d80f6c6216221
Daniel Yeo <dan@dkcy.com>
2016-12-16 09:04:11 +0000

    first upload

---
+++ footer.php
@@ -0,0 +1,1 @@
+<div id="footer"><span class="left"><a href="<?php echo get_permalink(439); ?>">About</a> | <?php wp_loginout(); ?> | <a href="<?php bloginfo('rss2_url'); ?>">Subscribe (RSS)</a></span><span class="right">Powered by <a href="http://www.wordpress.org/">Wordpress 3.5.1</a>. Site design © 2008 <a href="mailto:design@dkcy.com">dkcy</a></span></div>
---
+++ single.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+get_header();
+?>
+<div id="content">
+
+<div id="main_content" class="main">
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<div class="navigation">
+			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
+			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
+		</div>
+
+		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<h2><?php the_title(); ?></h2>
+<small><?php the_time('l, F jS, Y') ?></small>
+			<div class="entry">
+				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
+
+				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+				<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
+
+				<p class="postmetadata alt">
+					<small>
+						This entry is filed under <?php the_category(', ') ?>.
+						You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
+
+						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+							// Both Comments and Pings are open ?>
+							You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
+
+						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+							// Only Pings are Open ?>
+							Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
+
+						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+							// Comments are open, Pings are not ?>
+							You can skip to the end and leave a response. Pinging is currently not allowed.
+
+						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+							// Neither Comments, nor Pings are open ?>
+							Both comments and pings are currently closed.
+
+						<?php } edit_post_link('Edit this entry','','.'); ?>
+
+					</small>
+				</p>
+
+			</div>
+		</div>
+
+	<?php comments_template(); ?>
+
+	<?php endwhile; else: ?>
+
+		<p>Sorry, no posts matched your criteria.</p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
+	</div>
+
+<?php get_sidebar(); ?>
+</div>
